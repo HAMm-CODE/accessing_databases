b0fc26fabdfc10ee5eb8886b1b6583fbb4cf0952
Hamm-code <sentsdrimma123@gmail.com>
2026-05-14 14:41:52 +0300

    add query execution and output display for INSTRUCTOR table

--- accessing_databases.py
+++ accessing_databases.py
@@ -10,3 +10,8 @@ df = pd.read_csv(file_path, names = attribute_list)
 
 df.to_sql(table_name, conn, if_exists='replace', index=False)
 print('Table is ready')
+
+query_statement = f"SELECT * FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
